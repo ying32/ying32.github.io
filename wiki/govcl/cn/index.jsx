d1566b6651156805df36c02b2280e34450279e43
ying32 <ying_33@live.cn>
2019-04-23 18:59:28 +0800

    更新代码高亮背景色

--- wiki/govcl/cn/index.jsx
+++ wiki/govcl/cn/index.jsx
@@ -13,9 +13,17 @@ const { Header, Sider, Content, Footer } = Layout;
 
 // // import zhCN from 'antd/lib/locale-provider/zh_CN';
 // const zhCN = antd.zh_CN;
+
 marked.setOptions({
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
     highlight: function (code) {
-    return hljs.highlightAuto(code).value;
+      return hljs.highlightAuto(code).value;
   }
 });
  
