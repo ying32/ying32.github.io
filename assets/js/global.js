d1f1a9aa45ca4ffe27f745799f315cb81512aa0e
不在乎y <ying_33@live.cn>
2023-07-16 13:49:17 +0800

    update

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,6 +6,10 @@ function loadRepoInfo() {
     $.get('https://api.github.com/repos/ying32/govcl/releases/latest', function(res){
         $('#btnDownload').attr("href", res.assets[0].browser_download_url);
         $("#versionInfo").html(res.tag_name);
+		if(res.assets.length > 1)
+            $("#btnDownloadUIDesigner").attr("href", res.assets[1].browser_download_url);
+		else
+			$("#btnDownloadUIDesigner").attr("href", "javascript:alert('No designer is available');");
     }, 'json');
 
     $.get('https://api.github.com/repos/ying32/govcl', function(res){
