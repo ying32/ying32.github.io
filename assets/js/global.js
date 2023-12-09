0483e20a35a42989b8c567f81633a8e601dac800
不在乎y <ying_33@live.cn>
2023-12-09 12:15:02 +0800

    update

--- assets/js/global.js
+++ assets/js/global.js
@@ -4,20 +4,15 @@
 
 function loadRepoInfo() {
     $.get('https://api.github.com/repos/ying32/govcl/releases/latest', function(res){
-		// https://github.com/ying32/govcl/releases/download/v2.2.3/liblcl-2.2.3.zip
 		for(var i=0;i<res.assets.length;i++){
 			if(res.assets[i].name.indexOf("liblcl") != -1){
 				$('#btnDownload').attr("href", res.assets[i].browser_download_url);
-				break;
+			} else if(res.assets[i].name.indexOf("GoVCLDesigner-win") != -1){
+				$('#btnDownloadUIDesigner').attr("href", res.assets[i].browser_download_url);
 			}		
 		}
         
         $("#versionInfo").html(res.tag_name);
-		//if(res.assets.length > 1)
-        //    $("#btnDownloadUIDesigner").attr("href", res.assets[1].browser_download_url);
-		//else
-		//	$("#btnDownloadUIDesigner").attr("href", "javascript:alert('No designer is available');");
-		$("#btnDownloadUIDesigner").attr("href", "https://github.com/ying32/govcl/releases/download/v2.2.3/GoVCLDesigner-win-1.2.0.zip");
     }, 'json');
 
     $.get('https://api.github.com/repos/ying32/govcl', function(res){
